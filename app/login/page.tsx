a028abfe6b132aee598502fcf4ee0e866b3cc43c
hellohendri <hendrinarendra@gmail.com>
2024-02-12 00:25:34 +0700

    fix: fixing login form position

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/app/ui/loginForm";
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <main className="flex flex-col items-center justify-center md:h-screen md:w-screen">
         <LoginForm />
     </main>
   );
